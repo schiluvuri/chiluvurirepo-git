57064da30dda5478bd5e87db7d32104c11bbcdf3
Raju Chiluvuri <schiluvuri@gmail.com>
2021-11-30 12:02:15 -0500

    commiting first git code

---
+++ provider.tf
@@ -0,0 +1,11 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA2BBKESPYNYQ2RIU5"
+  secret_key = "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"
+}
+/*
+provider "github" {
+  token = "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"
+}
+*/
